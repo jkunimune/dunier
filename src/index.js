6f42dd0921b48dea8bebc078008e7c90b16f2d6f
Justin Kunimune <justinkunimune@gmail.com>
2019-11-11 10:50:35 -0500

    Enter the third dimension
    I made a 3d plot using plot.ly. It's sort of a sphere.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,38 @@
+// index.js: interfaces with forms and plots
+'use strict';
+
 /**
  * Once the page is ready, start the algorithm!
  */
 $( document ).ready(function() {
 	console.log("ready!");
-	planet_map = document.getElementById('planet-map');
-	data = [{
-		x: [1, 2, 3, 4, 5],
-		y: [1, 2, 4, 8, 16] }]
-	layout = {
-		margin: { t: 0, b: 0, l: 0, r: 0 } }
-	Plotly.plot(planet_map, data, layout, {responsive: true});
 });
 
 /**
- * Generate the planet and its temperature and rainfall.
+ * Generate the planet and its mean temperature (not yet accounting for altitude)
  */
 $( "#planet-apply" ).on("click", function() {
-	console.log("planet!");
-});
+	const mapDiv = document.getElementById('planet-map');
+	const data = [{
+		type: "mesh3d",
+		x: [], y: [], z: [],
+		intensity: [],
+	}];
+	const layout = {
+		margin: {l: 20, r: 20, t: 20, b: 20},
+	};
+	const config = {
+		responsive: true,
+	};
 
+	for (let i = 0; i < 6; i ++) {
+		let ph = Math.asin(Math.random());
+		let l = Math.PI*(2*Math.random() - 1);
+		data[0].x.push(Math.sin(ph)*Math.cos(l));
+		data[0].y.push(Math.sin(ph)*Math.sin(l));
+		data[0].z.push(Math.cos(ph));
+		data[0].intensity.push(Math.random());
+	}
+
+	Plotly.react(mapDiv, data, layout, config);
+});
