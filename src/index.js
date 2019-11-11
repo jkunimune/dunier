aef38054a074960f0daca3f6f5a75e1ea7b8c84d
Justin Kunimune <justinkunimune@gmail.com>
2019-11-10 22:47:34 -0500

    I _knew_ you could hear me!
    I added responsiveness. In the form of logs. This is great. I didn't know how to do any of this.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
-planet_map = document.getElementById('planet-map');
-data = [{
-	x: [1, 2, 3, 4, 5],
-	y: [1, 2, 4, 8, 16] }]
-layout = {
-	margin: { t: 0 } }
-Plotly.plot(planet_map, data, layout, {responsive: true});
+/**
+ * Once the page is ready, start the algorithm!
+ */
+$( document ).ready(function() {
+	console.log("ready!");
+	planet_map = document.getElementById('planet-map');
+	data = [{
+		x: [1, 2, 3, 4, 5],
+		y: [1, 2, 4, 8, 16] }]
+	layout = {
+		margin: { t: 0, b: 0, l: 0, r: 0 } }
+	Plotly.plot(planet_map, data, layout, {responsive: true});
+});
+
+/**
+ * Generate the planet and its temperature and rainfall.
+ */
+$( "#planet-apply" ).on("click", function() {
+	console.log("planet!");
+});
+
