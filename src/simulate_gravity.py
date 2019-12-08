3a63571f4c18805debd1a6ef76715be51df68d57
Justin Kunimune <justinkunimune@gmail.com>
2019-12-08 16:39:38 -0500

    WHEEEEEEEEEEEEEEEEEEE
    I made a fun animation. It's going to go away soon, but I'm really proud of it.

--- src/simulate_gravity.py
+++ src/simulate_gravity.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from scipy.special import jv, jn_zeros
 
 RES = 12
-MAX_ASPECT_RATIO = 1.5
+MAX_ASPECT_RATIO = 2
 BOUNDARY_EXCESS = 3
 EIGEN_RES = 36
 INTEGRATION_RES = 8
@@ -23,7 +23,7 @@ z_in_pixel = np.linspace(-dz/2, dz/2, INTEGRATION_RES+1)[:-1] + dz/INTEGRATION_R
 P_in_pixel, Z_in_pixel = np.meshgrid(ρ_in_pixel, z_in_pixel)
 n_in_pixel = P_in_pixel.size
 
-print("forming basis")
+print("sana baze")
 
 max_n = int(EIGEN_RES*MAX_ASPECT_RATIO) # for relevant ns and ms
 max_m = int(EIGEN_RES)
@@ -45,27 +45,55 @@ for n in range(0, max_n):
 
 print("iterating over angular velocities")
 
-for ω2 in [1/6]:#np.linspace(0, 1, 6): # angular kinetic energy
-	# for i in range(max_n*max_m):
+rotation_parameters = []
+aspect_ratios = []
+for ω2 in np.linspace(0, 0.5, 12): # angular velocity
+	print("solving for equilibrium: ")
 
-		print("solving for potential")
+	planet = np.zeros(P.shape) # initialize a spherical planet
+	planet[np.hypot(P, Z) < 1] = 1
 
-		planet = np.zeros(P.shape)
-		planet[np.hypot(P, Z) < 1] = 1 # define the initial planet
-
+	while True:
 		A = np.sum(PB*planet[np.newaxis, :, :]*dρ*dz, axis=(1,2)) # fourier transform
 		planet_twiddle = np.sum(B*A[:, np.newaxis, np.newaxis], axis=0)
 
 		ɸ = np.sum(B*(A/Λ)[:, np.newaxis, np.newaxis], axis=0) # compute the potential
 		g = np.linalg.norm(np.gradient(ɸ, z, ρ), axis=0)
-		ɸ /= np.max(g[:-1,:-1])
+		ɸ /= np.max(g[:-1,:-1]) # normalize so surface gravity is 1
 
 		ɸ += ω2*P**2/2 # introduce the effective rotational potential
 
-		print("numografa")
-
-		plt.pcolormesh(ρ_mesh, z_mesh, planet_twiddle)
+		plt.clf()
+		plt.pcolormesh(ρ_mesh, z_mesh, planet)
 		plt.colorbar()
 		plt.contour(ρ, z, ɸ, levels=12, colors='w')
 		plt.axis('equal')
-		plt.show()
+		plt.pause(.5)
+
+		edge = np.nonzero(np.linalg.norm(np.gradient(planet), axis=0)) # redistribute the mass
+		num_rocks = int(np.sum(planet[edge])) # this could be run multiple times to get an exact equipotential solution before recomputing the potential,
+		hierarchy = np.argsort(-ɸ[edge]) # but this way is a little stabler, and I think a little faster
+		if np.all(planet[edge[0][hierarchy[:num_rocks]],edge[1][hierarchy[:num_rocks]]] == 1): # if this changes absolutely noting, break
+			break
+		planet[edge[0][hierarchy[:num_rocks]],edge[1][hierarchy[:num_rocks]]] = 1 # WHEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
+		planet[edge[0][hierarchy[num_rocks:]],edge[1][hierarchy[num_rocks:]]] = 0 # EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
+
+	print("evaluating")
+	max_radius = np.max(P[np.nonzero(planet)]) + dρ/2
+	min_radius = np.max(Z[np.nonzero(planet)]) + dz/2
+	g = 1 # (because I normalized it)
+	rotation_parameters.append(ω2*max_radius/g)
+	aspect_ratios.append(max_radius/min_radius)
+
+	print("numografa")
+
+	# plt.pcolormesh(ρ_mesh, z_mesh, planet)
+	# plt.colorbar()
+	# plt.contour(ρ, z, ɸ, levels=12, colors='w')
+	# plt.axis('equal')
+	plt.show()
+
+print(np.array(rotation_parameters))
+print(np.array(aspect_ratios))
+plt.plot(rotation_parameters, aspect_ratios)
+plt.show()
