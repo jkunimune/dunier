c72395d8612aa03f2e1e777af2ba5683249cabb1
Justin Kunimune <justinkunimune@gmail.com>
2020-07-04 10:51:22 -0400

    Localization!
    I support multiple languages now! I moved all of the text in the HTML file into a JSON file, made more JSON files with languages other than English, wrote a short Python script to generate localized HTML files from template, and updated the header to include a dropdown that lets one switch between them! I think the only thing that could make this better is cute flag circles.

---
+++ src/build_html.py
@@ -0,0 +1,26 @@
+import json
+import os
+import re
+
+LANGUAGES = ['en', 'es', 'jp', 'pd']
+DEFAULT_LANGUAGE = 'pd'
+
+for filename in os.listdir('../res/templates/'):
+	print(filename)
+	for lang_code in LANGUAGES:
+		print(lang_code)
+		with open(f'../res/templates/{filename}', 'r', encoding='utf8') as page_file:
+			page = page_file.read()
+		with open(f'../res/tarje/{lang_code}.json', 'r', encoding='utf8') as lang_file:
+			lang = json.load(lang_file)
+		for key, value in lang.items():
+			page = page.replace(f'{{{{{key}}}}}', value)
+		remaining_keys = re.search(r'\{\{([a-z-.]+)\}\}', page)
+		if remaining_keys:
+			raise KeyError(f"no jana cabe {remaining_keys.group(1)}!")
+		page = re.sub(r'\{\{.*\}\}', "MISSINGNO", page)
+		with open(f'../bax/{lang_code}/{filename}', 'w', encoding='utf8') as page_file:
+			page_file.write(page)
+		if lang_code == DEFAULT_LANGUAGE:
+			with open(f'../{filename}', 'w', encoding='utf8') as page_file:
+				page_file.write(page)
