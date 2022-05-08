d1e97b6df7a22560f9821bd7f395c746fbc25744
Justin Kunimune <justinkunimune@gmail.com>
2022-05-08 11:07:56 -0400

    fake jinja
    I made my html templates inherit from a base template.  now they are all more consistent with each other.  it also made it much easier to add a footer, which now includes a public domain dedication notice.  I mite need to replace all of my MIT licenses in my code with unlicenses now... I'm not sure, I really don't understand copyrite law.  it probably doesn't matter either way.  I'll think about it.

--- src/python/build_html.py
+++ src/python/build_html.py
@@ -5,24 +5,45 @@ import re
 LANGUAGES = ['en', 'es', 'jp', 'pd']
 DEFAULT_LANGUAGE = 'pd'
 
+# load the base
+with open(f'../../res/templates/base.html', 'r', encoding='utf8') as base_file:
+	base = base_file.read()
+
+# iterate thru all non-base templates in the folder
 for filename in os.listdir('../../res/templates/'):
+	if filename == "base.html" or not filename.endswith(".html"):
+		continue
+	filename = filename[:-5]
+
 	print(f"{filename}")
+	# iterate thru the languages
 	for lang_code in LANGUAGES:
+		# load the template and insert it into the base
 		print(f"  {lang_code}")
-		with open(f'../../res/templates/{filename}', 'r', encoding='utf8') as page_file:
-			page = page_file.read()
+		with open(f'../../res/templates/{filename}.html', 'r', encoding='utf8') as page_file:
+			page = base.replace('{Content}', page_file.read())
+
+		# replace the special key
+		page = page.replace(f'{{.name}}', filename)
+
+		# replace the basic keys
 		with open(f'../../res/tarje/{lang_code}.json', 'r', encoding='utf8') as lang_file:
 			lang = json.load(lang_file)
 		for key, value in lang.items():
-			page = page.replace(f'{{{{{key}}}}}', value)
-		remaining_keys = re.search(r'\{\{([a-zA-Z0-9-.]+)\}\}', page)
+			page = page.replace(f'{{{key}}}', value)
+		remaining_keys = re.search(r'{([a-zA-Z0-9-.]+)}', page)
 		if remaining_keys:
-			raise KeyError(f"no jana cabe {remaining_keys.group(1)}!")
-		page = re.sub(r'\{\{.*\}\}', "MISSINGNO", page)
-		with open(f'../../bash/{lang_code}/{filename}', 'w', encoding='utf8') as page_file:
+			raise KeyError(f"no jana cabe '{remaining_keys.group(1)}'!")
+
+		# resolve any if-statements
+		page = re.sub(fr'{{If"{filename}"([^}}]*)}}', '\\1', page)
+		page = re.sub(fr'{{If"[^"]*"[^}}]*}}', '', page)
+
+		# save the result
+		with open(f'../../bash/{lang_code}/{filename}.html', 'w', encoding='utf8') as page_file:
 			page_file.write(page)
 		if lang_code == DEFAULT_LANGUAGE:
-			with open(f'../../{filename}', 'w', encoding='utf8') as page_file:
+			with open(f'../../{filename}.html', 'w', encoding='utf8') as page_file:
 				page_file.write(page)
 
 print("fini!")
