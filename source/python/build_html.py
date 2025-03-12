b80c8413e08050e7b18cda20d57a7b677ff764dc
Justin Kunimune <justinkunimune@gmail.com>
2025-03-12 16:38:40 -0400

    remove jQuery and streamline input spinners
    I went thru and finally fully removed jquery as a dependency.  this turnd out to be a bigger change than I expected.  it was easy enuff to change the parts of my code that used jquery, but it turns out that bootstrap-input-spinner requires jquery.  of course, changing the parts of that code that used over to equivalent vanilla javascript functions was also not a big deal.  the real problem was that I noticed that bootstrap-input-spinner works in a fundamentally weird way that I don't like, so I couldn't help myself from restructuring the whole thing.
    
    you see, bootstrap-input-spinner is made to work as a script that you tack onto an existing HTML document to make the spinners look pretty.  but for varius reasons the fancy spinners with which you end up aren't actually numeric inputs, but text inputs.  the script can't change the existing numeric inputs to text ones without altering the behavior of any scripts that were expecting to interface with those spinners, so it instead creates a new fancy "spinner" (text input) right next to the original spinner, hides the original spinner, and then binds them together so that any changes in one are reflected in the other.
    
    this works, but I don't like it.  for one, I'm not fully convinced that it will work smoothly with screen-readers.  also, while the script does its best, it doesn't cover all bilt-in functionality of numeric inputs, like the user being able to use the up and down arrow keys to increment and decrement the value.  it's also just weird!
    
    so I changed it to a totally different regime where the fancy HTML element, with the auxiliary buttons and such, is bilt in build_html.py, and thus doesn't need to be constructed by javascript after page-load.  this naturally alleviates the need to keep an original element alongside a newly constructed one.  most importantly, the core element is now a numeric input.  this imposes some limitations – I can't have thousands separators anymore, and the CSS to hide the efault arrow buttons is more complicated.  but it also adds arrow key functionality and greatly reduces the amount of necessary auxiliary code.  you no longer need code to bild the element because that's all in the python script now.  you no longer need to link the two elements because there's only one element.  you no longer need to propagate events between them for the same reason. you no longer need to watch for as many events because most of those are handled automaticly.  you no longer need to do as much locale-aware number formatting because it turns out the bilt-in inputs already do that.  really the auxiliary code now only exists to enforce consistent number formatting (which bilt-in number inputs don't do well) and assine functionality to the + and − buttons.
    
    since this put the complexity of built_html.py over what I consider acceptable for the functionless format, I broke it all up into functions.  the only behavioral change is that there's now a {Build} command that can be used to make fancy spinners.
    
    it also now uses a minus sign by default instead of a hyphen-minus by default.

--- source/python/build_html.py
+++ source/python/build_html.py
@@ -9,59 +9,138 @@ import re
 LANGUAGES = ['en', 'es', 'ja', 'pd']
 DEFAULT_LANGUAGE = 'pd'
 
-with open(f'../../package.json', 'r', encoding='utf8') as package_file:
-	package = json.loads(package_file.read())
-	version = package["version"]
 
-# load the base
-with open(f'../../templates/base.html', 'r', encoding='utf8') as base_file:
-	base = base_file.read()
+def build_html():
+	# load the version number
+	with open(f'../../package.json', 'r', encoding='utf8') as package_file:
+		package = json.loads(package_file.read())
+		version = package["version"]
 
-# iterate thru all non-base templates in the folder
-for filename in os.listdir('../../templates/'):
-	if filename == "base.html" or not filename.endswith(".html"):
-		continue
-	filename = filename[:-5]
-	print(f"{filename}")
+	# load the base
+	with open(f'../../templates/base.html', 'r', encoding='utf8') as base_file:
+		base = base_file.read()
 
-	# load the template and insert it into the base
-	with open(f'../../templates/{filename}.html', 'r', encoding='utf8') as page_file:
-		template = base.replace('{Content}', page_file.read())
+	# iterate thru all non-base templates in the folder
+	for filename in os.listdir('../../templates/'):
+		if filename == "base.html" or not filename.endswith(".html"):
+			continue
+		filename = filename[:-5]
+		print(f"{filename}")
 
+		# load the template and insert it into the base
+		with open(f'../../templates/{filename}.html', 'r', encoding='utf8') as page_file:
+			template = base.replace('{Content}', page_file.read())
+
+		# iterate thru the languages
+		for lang_code in LANGUAGES:
+			print(f"  {lang_code}")
+
+			page = template
+
+			page = replace_keys(page, lang_code, filename, version)
+
+			page = resolve_if_statements(page)
+
+			page = resolve_build_commands(page)
+
+			save_page(page, lang_code, filename)
+
+	print("fini!")
+
+
+def replace_keys(page: str, lang_code: str, filename: str, version: str) -> str:
 	# replace the special keys
-	template = template.replace(f'{{.name}}', filename)
-	template = template.replace(f'{{.version}}', version)
+	page = page.replace(f'{{.name}}', filename)
+	page = page.replace(f'{{.version}}', version)
 
-	# iterate thru the languages
-	for lang_code in LANGUAGES:
-		print(f"  {lang_code}")
-		# replace the basic keys
-		with open(f'../../resources/translations/{lang_code}.ts', 'r', encoding='utf8') as lang_file:
-			lang = json.loads(lang_file.read()[15:-2])
-		page = template
-		for key, value in lang.items():
-			page = page.replace(f'{{{key}}}', value)
-		remaining_keys = re.search(r'[^$]{([a-z.][a-zA-Z0-9-._]+)}', page)
-		if remaining_keys:
-			raise KeyError(f"no jana cabe '{remaining_keys.group(1)}'!")
+	# replace the basic keys
+	with open(f'../../resources/translations/{lang_code}.ts', 'r', encoding='utf8') as lang_file:
+		lang = json.loads(lang_file.read()[15:-2])
+	for key, value in lang.items():
+		page = page.replace(f'{{{key}}}', value)
+	remaining_keys = re.search(r'[^$]{([a-z.][a-zA-Z0-9-._]+)}', page)
+	if remaining_keys:
+		raise KeyError(f"no jana cabe '{remaining_keys.group(1)}'!")
 
-		# resolve any if-statements
-		for if_statement in reversed(list(re.finditer(fr'{{If ([^ }}]*) ([^ }}]*)}}([^}}]*){{EndIf}}', page))):
-			a, b, body = if_statement.groups()
-			if a == b:
-				page = page[:if_statement.start()] + body + page[if_statement.end():]
+	return page
+
+
+def resolve_if_statements(page: str) -> str:
+	# resolve any if-statements
+	for if_statement in reversed(list(re.finditer(fr'{{If ([^ }}]*) ([^ }}]*)}}([^}}]*){{EndIf}}', page))):
+		a, b, body = if_statement.groups()
+		if a == b:
+			page = page[:if_statement.start()] + body + page[if_statement.end():]
+		else:
+			page = page[:if_statement.start()] + page[if_statement.end():]
+	remaining_if_statements = re.search(fr'{{If([^}}]*)', page)
+	if remaining_if_statements:
+		raise ValueError(f"could not parse the if-statement {remaining_if_statements.group()}")
+	return page
+
+
+def resolve_build_commands(page: str) -> str:
+	# resolve any calls to the build command
+	for call in reversed(list(re.finditer(fr'{{Build ([^}}]*)}}', page))):
+		arguments = {}
+		for key_value_pair in call.group(1).split(" "):
+			parsing = re.fullmatch(r'([a-z-]+)="([^"]*)"', key_value_pair)
+			if parsing is None:
+				raise ValueError(f"could not parse the key-value pair string '{key_value_pair}'")
 			else:
-				page = page[:if_statement.start()] + page[if_statement.end():]
-		remaining_if_statements = re.search(fr'{{If([^}}]*)', page)
-		if remaining_if_statements:
-			raise ValueError(f"could not parse the if-statement {remaining_if_statements.group()}")
+				key, value = parsing.groups()
+				arguments[key] = value
 
-		# save the result
-		os.makedirs(f"../../{lang_code}/", exist_ok=True)
-		with open(f'../../{lang_code}/{filename}.html', 'w', encoding='utf8') as page_file:
+		if arguments["type"] == "spinner":
+			content = build_fancy_input_spinner(
+				identifier=arguments["id"],
+				value=float(arguments["value"]),
+				minimum=float(arguments["min"]),
+				maximum=float(arguments["max"]),
+				step=float(arguments["step"]),
+				decimals=int(arguments.get("decimals", "0")),
+				suffix=arguments.get("suffix", ""),
+			)
+		else:
+			raise ValueError(f'unrecognized build type: "{arguments["type"]}"')
+
+		page = page[:call.start()] + content + page[call.end():]
+	return page
+
+
+def save_page(page: str, lang_code: str, filename: str):
+	# save the result
+	os.makedirs(f"../../{lang_code}/", exist_ok=True)
+	with open(f'../../{lang_code}/{filename}.html', 'w', encoding='utf8') as page_file:
+		page_file.write(page)
+	if lang_code == DEFAULT_LANGUAGE:
+		with open(f'../../{filename}.html', 'w', encoding='utf8') as page_file:
 			page_file.write(page)
-		if lang_code == DEFAULT_LANGUAGE:
-			with open(f'../../{filename}.html', 'w', encoding='utf8') as page_file:
-				page_file.write(page)
 
-print("fini!")
+
+def build_fancy_input_spinner(
+		identifier: str, value: float,
+		minimum: float, maximum: float, step: float,
+		decimals=0, prefix="", suffix="") -> str:
+	return (
+		'<div class="input-group fancy-spinner">\n' +
+		'  <div class="input-group-prepend">\n' +
+		'    <button style="min-width:2.5rem" class="btn btn-decrement btn-outline-secondary" type="button">\n' +
+		'      <strong>&minus;</strong>\n' +
+		'    </button>\n' +
+		(f'    <span class="input-group-text">{prefix}</span>\n' if prefix else '') +
+		'  </div>\n' +
+		'  <input type="number" inputmode="decimal" style="text-align:center" class="form-control" ' +
+		f'id="{identifier}" value="{value}" data-decimals="{decimals}" min="{minimum}" max="{maximum}" step="{step}"/>\n' +
+		'  <div class="input-group-append">' +
+		(f'    <span class="input-group-text">{suffix}</span>\n' if suffix else '') +
+		'    <button style="min-width:2.5rem" class="btn btn-increment btn-outline-secondary" type="button">\n' +
+		'      <strong>&plus;</strong>\n' +
+		'    </button>\n' +
+		'  </div>\n' +
+		'</div>'
+	)
+
+
+if __name__ == "__main__":
+	build_html()
