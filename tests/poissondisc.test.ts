e67c9b5ba2d582c5484179b462a32f54d7faf044
Justin Kunimune <justinkunimune@gmail.com>
2025-07-03 19:37:33 -0400

    fix Poisson disc test
    now that the Poisson disc  sampling has consistent sampling at the borders rather than squeezing in as many discs as is theoreticly possible, it is possible for this test to return far fewer points.

--- tests/poissondisc.test.ts
+++ tests/poissondisc.test.ts
@@ -8,14 +8,14 @@ import {Random} from "../source/utilities/random.js";
 test("maximal sampling", () => {
 	const path = [
 		{type: 'M', args: [0, 0]},
-		{type: 'L', args: [0, 2]},
-		{type: 'L', args: [2, 2]},
-		{type: 'L', args: [2, 0]},
+		{type: 'L', args: [0, 4]},
+		{type: 'L', args: [4, 4]},
+		{type: 'L', args: [4, 0]},
 		{type: 'L', args: [0, 0]},
 	];
 	const points = poissonDiscSample(path, Infinity, 1, new Random(0));
 	expect(points.length).toBeGreaterThanOrEqual(4);
-	expect(points.length).toBeLessThanOrEqual(9);
+	expect(points.length).toBeLessThanOrEqual(23);
 	for (let i = 0; i < points.length; i ++)
 		for (let j = 0; j < i; j ++)
 			expect(Math.hypot(points[i].x - points[j].x, points[i].y - points[j].y)).toBeGreaterThanOrEqual(1);
