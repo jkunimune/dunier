96039047db88ff7bd76907be3d8948b388df71af
Justin Kunimune <justinkunimune@gmail.com>
2025-10-26 12:32:26 -0400

    make texture look a little bit better
    I made trees have more uniform height so that they don't overlap quite so much, made mountains steeper so that they appear to take up more space, and added multiple sizes of mountain so that more of them can be placed.  I also reduced the number of meranti trees because I think they look best when used sparingly.

--- resources/texture_mixes.ts
+++ resources/texture_mixes.ts
@@ -33,38 +33,40 @@ export default [
   {
     "name": "hill",
     "components": [
-      {"name": "hill_0", "density": 0.003, diameter: 8},
-      {"name": "hill_1", "density": 0.003, diameter: 8},
-      {"name": "hill_2", "density": 0.003, diameter: 8},
-      {"name": "hill_3", "density": 0.003, diameter: 8},
-      {"name": "mountain_0", "density": 0.003, diameter: 12},
-      {"name": "mountain_1", "density": 0.003, diameter: 12},
-      {"name": "mountain_2", "density": 0.003, diameter: 12},
-      {"name": "mountain_3", "density": 0.003, diameter: 12}
+      {"name": "hill_0", "density": 0.006, diameter: 6},
+      {"name": "hill_1", "density": 0.006, diameter: 6},
+      {"name": "mountain_0", "density": 0.003, diameter: 8},
+      {"name": "mountain_1", "density": 0.003, diameter: 8},
+      {"name": "mountain_2", "density": 0.003, diameter: 8},
+      {"name": "mountain_3", "density": 0.003, diameter: 8}
     ]
   },
   {
     "name": "jungle",
     "components": [
-      {"name": "banyan_0", "density": 0.08, diameter: 7},
-      {"name": "banyan_1", "density": 0.08, diameter: 7},
-      {"name": "meranti_0", "density": 0.04, diameter: 3},
-      {"name": "meranti_1", "density": 0.04, diameter: 3},
-      {"name": "monkeypod_0", "density": 0.06, diameter: 5},
-      {"name": "monkeypod_1", "density": 0.06, diameter: 6},
+      {"name": "banyan_0", "density": 0.10, diameter: 7},
+      {"name": "banyan_1", "density": 0.10, diameter: 7},
+      {"name": "meranti_0", "density": 0.02, diameter: 3},
+      {"name": "meranti_1", "density": 0.02, diameter: 3},
+      {"name": "monkeypod_0", "density": 0.08, diameter: 5},
+      {"name": "monkeypod_1", "density": 0.08, diameter: 6},
       {"name": "oak_0", "density": 0.06, diameter: 6},
       {"name": "oak_1", "density": 0.06, diameter: 4},
-      {"name": "palm_0", "density": 0.06, diameter: 5},
-      {"name": "palm_1", "density": 0.06, diameter: 5}
+      {"name": "palm_0", "density": 0.08, diameter: 5},
+      {"name": "palm_1", "density": 0.08, diameter: 5}
     ]
   },
   {
     "name": "mountain",
     "components": [
-      {"name": "mountain_0", "density": 0.05, diameter: 12},
-      {"name": "mountain_1", "density": 0.05, diameter: 12},
-      {"name": "mountain_2", "density": 0.05, diameter: 12},
-      {"name": "mountain_3", "density": 0.05, diameter: 12}
+      {"name": "mountain_0", "density": 0.03, diameter: 8},
+      {"name": "mountain_1", "density": 0.03, diameter: 8},
+      {"name": "mountain_2", "density": 0.03, diameter: 8},
+      {"name": "mountain_3", "density": 0.03, diameter: 8},
+      {"name": "mountain_4", "density": 0.06, diameter: 12},
+      {"name": "mountain_5", "density": 0.06, diameter: 12},
+      {"name": "mountain_6", "density": 0.06, diameter: 12},
+      {"name": "mountain_7", "density": 0.06, diameter: 12}
     ]
   },
   {
