8d6f040adc0b2ec5a3bca0c71bb646520e2ae5fb
Justin Kunimune <justinkunimune@gmail.com>
2025-10-30 21:53:49 -0400

    change color scheme
    the colors have somewhat higher contrast now in the political map.  also political is now the default because I suspect it's what peeple most expect to see.

--- source/python/generate_colors.py
+++ source/python/generate_colors.py
@@ -6,51 +6,59 @@ from colour import convert
 import numpy as np
 import matplotlib.pyplot as plt
 
-N = 36
-min_L = .75
-max_L = .90
-r = .09
+min_L = .79
+max_L = .87
+r = .08
 ɸ = 0
-# Z_PERIOD = np.linspace(3, 9, 601)
-Z_PERIOD = [3.89]
+stride = 9
+z_period = 3
 
-badnesses = []
-for z_period in Z_PERIOD:
-	angles = []
-	heights = []
-	colors = []
-	for i in range(N):
-		θ = i*2*np.pi*(1 + np.sqrt(5))/2 + ɸ
-		z = (i+1)/z_period%1
-		z = z*(max_L - min_L) + min_L
-		x = r*np.cos(θ)
-		y = r*np.sin(θ)
-		oklab = (z, x, y)
+golden_angle = 2*np.pi*(1 + np.sqrt(5))/2
 
-		if np.cos(θ - np.radians(229)) > .8 and z > .85:
-			continue  # skip light blues
 
-		rgb = convert((z, x, y), "Oklab", "sRGB")
-		if len(Z_PERIOD) == 1:
-			print("'rgb({:.0f}, {:.0f}, {:.0f})',".format(
-				256*rgb[0], 256*rgb[1], 256*rgb[2]))
+def angular_distance(a, b):
+	return abs((a - b + 180)%360 - 180)
 
-		angles.append(np.degrees(θ)%360)
-		heights.append(z)
-		colors.append(oklab)
-	badness = 0
-	for i in range(1, len(colors)):
-		badness += 1/np.min([np.sqrt(np.sum((np.subtract(colors[i], colors[j]))**2)) for j in range(len(colors)) if j < i])
-	badnesses.append(badness)
 
-if len(Z_PERIOD) > 1:
-	print(f"maxim bon zamane: {Z_PERIOD[np.argmin(badnesses)]}")
-	plt.scatter(Z_PERIOD, badnesses)
-	plt.xlabel("z zamane")
-	plt.ylabel("malia")
-else:
-	print(f"malia da zamane: {badness}")
-	plt.scatter(angles, heights, c=range(len(angles)))
-	plt.xlabel("gone (°)")
-	plt.ylabel("gawia (%)")
+angles = []
+heights = []
+colors = []
+valid = []
+for i in range(stride*z_period):
+	θ = i*golden_angle + ɸ
+	z = i/(stride*z_period - 1)*(max_L - min_L) + min_L
+	x = r*np.cos(θ)
+	y = r*np.sin(θ)
+	rgb = convert((z, x, y), "Oklab", "sRGB")
+	rgb = np.clip(rgb, 0, 1)
+	oklab = convert(rgb, "sRGB", "Oklab")
+
+	distance_from_water = np.sqrt((z - .949)**2 + (x + 0.016)**2 + (y + 0.019)**2)
+
+	angles.append(np.degrees(θ)%360)
+	heights.append(z)
+	colors.append(rgb)
+	valid.append(distance_from_water > 0.1)  # be wary of light blues
+
+# try to find an order that makes the first few colors very spread out
+order = np.arange(len(colors))
+# runs = list(order.reshape((z_period, stride)).transpose())
+# order = runs.pop(0)
+# while len(runs) > 0:
+# 	desired_next_angle = np.degrees(len(order)*ɸ + golden_angle)
+# 	error = [angular_distance(angles[run[0]], desired_next_angle) for run in runs]
+# 	order = np.append(order, runs.pop(np.argmin(error)))
+order = order[np.array(valid)[order]]
+angles = np.array(angles)[order]
+heights = np.array(heights)[order]
+colors = np.array(colors)[order]
+
+for r, g, b in colors:
+	print(f"'rgb({int(256*r)}, {int(256*g)}, {int(256*b)})',")
+
+plt.scatter(angles, heights, c=colors)
+for i in range(len(colors)):
+	plt.text(angles[i], heights[i], f"{i}")
+plt.xlabel("hue (°)")
+plt.ylabel("lightness (%)")
 plt.show()
