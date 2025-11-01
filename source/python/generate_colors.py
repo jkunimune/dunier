9fcf23daab2bdbb1e8337e1d380848faf215ff58
Justin Kunimune <justinkunimune@gmail.com>
2025-11-01 13:49:58 -0400

    adjust color scheme again
    I really need to just write a smarter color placement algorithm because I have spent way too much time trying to design a colormap to work with my dumb scheme.  whatever, I think this looks nicer because now the text is easier to read because the trees are lighter because the colors are lighter because the ocean is lighter.

--- source/python/generate_colors.py
+++ source/python/generate_colors.py
@@ -6,12 +6,11 @@ from colour import convert
 import numpy as np
 import matplotlib.pyplot as plt
 
-min_L = .79
-max_L = .87
-r = .08
+min_L = .83
+max_L = .90
+r = .06
 ɸ = 0
-stride = 9
-z_period = 3
+N = 30
 
 golden_angle = 2*np.pi*(1 + np.sqrt(5))/2
 
@@ -24,30 +23,24 @@ angles = []
 heights = []
 colors = []
 valid = []
-for i in range(stride*z_period):
+for i in range(N):
 	θ = i*golden_angle + ɸ
-	z = i/(stride*z_period - 1)*(max_L - min_L) + min_L
+	z = i/(N - 1)*(max_L - min_L) + min_L
 	x = r*np.cos(θ)
 	y = r*np.sin(θ)
 	rgb = convert((z, x, y), "Oklab", "sRGB")
 	rgb = np.clip(rgb, 0, 1)
 	oklab = convert(rgb, "sRGB", "Oklab")
 
-	distance_from_water = np.sqrt((z - .949)**2 + (x + 0.016)**2 + (y + 0.019)**2)
+	distance_from_water = np.sqrt((z - .89)**2 + (x + 0.011)**2 + (y + 0.039)**2)
 
 	angles.append(np.degrees(θ)%360)
 	heights.append(z)
 	colors.append(rgb)
-	valid.append(distance_from_water > 0.1)  # be wary of light blues
+	valid.append(distance_from_water > 0.05)  # be wary of light blues
 
 # try to find an order that makes the first few colors very spread out
 order = np.arange(len(colors))
-# runs = list(order.reshape((z_period, stride)).transpose())
-# order = runs.pop(0)
-# while len(runs) > 0:
-# 	desired_next_angle = np.degrees(len(order)*ɸ + golden_angle)
-# 	error = [angular_distance(angles[run[0]], desired_next_angle) for run in runs]
-# 	order = np.append(order, runs.pop(np.argmin(error)))
 order = order[np.array(valid)[order]]
 angles = np.array(angles)[order]
 heights = np.array(heights)[order]
